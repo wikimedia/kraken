243aac0b304aa3373cacc318240e8b9c3a5f46a8
Andrew Otto <acotto@gmail.com>
2012-11-27 13:30:20 -0500

    updating index.html with storm links

--- src/php/index.php
+++ src/php/index.php
@@ -1,6 +1,7 @@
 <?php
 $namenode_host = "analytics1010.eqiad.wmnet";
 $frontend_host = "analytics1027.eqiad.wmnet";
+$storm_host    = "analtyics1002.eqiad.wmnet";
 ?>
 <html>
 <head>
@@ -10,7 +11,7 @@ $frontend_host = "analytics1027.eqiad.wmnet";
 <body>
 
 <div class="container">
-<h1> Kraken CDH4 Links</h1>
+<h1>Kraken CDH4 Links</h1>
 
 
 <h2>Hadoop</h2>
@@ -30,9 +31,14 @@ $frontend_host = "analytics1027.eqiad.wmnet";
 <li><a href="http://oozie.analytics.wikimedia.org/oozie">Oozie</a> (<a href="http://<?php echo $frontend_host ?>:11000/oozie/">internal</a>)</li>
 </ul>
 
+<h2>Storm</h2>
+<ul>
+<li><a href="http://storm.analytics.wikimedia.org">Storm</a> (<a href="http://<?php echo $storm_host ?>:6999">internal</a>)</li>
+</ul>
+
 <strong>NOTE:</strong><br/>In order for these URLs to work, add the following line to your /etc/hosts file:
 <pre>
-208.80.154.154 analytics.wikimedia.org namenode.analytics.wikimedia.org jobs.analytics.wikimedia.org history.analytics.wikimedia.org oozie.analytics.wikimedia.org hue.analytics.wikimedia.org
+208.80.154.154 analytics.wikimedia.org namenode.analytics.wikimedia.org jobs.analytics.wikimedia.org history.analytics.wikimedia.org oozie.analytics.wikimedia.org hue.analytics.wikimedia.org storm.analytics.wikimedia.org
 </pre>
 </div>
 
