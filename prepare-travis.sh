eb326543d8d89193a6bcd0edf9875d4f592ac30d
Diederik van Liere <dvanliere@wikimedia.org>
2013-02-21 14:28:13 -0500

    Oink another typo

--- prepare-travis.sh
+++ prepare-travis.sh
@@ -11,10 +11,10 @@ wget "$BASE_URL/hadoop/hadoop-common/$HADOOP_VERSION/hadoop-common-$HADOOP_VERSI
 wget "$BASE_URL/pig/pigunit/$PIG_VERSION/pigunit-$PIG_VERSION.jar";
 wget "$BASE_URL/pig/pig/$PIG_VERSION/pig-$PIG_VERSION.jar";
 
-mvn install:install-file -DgroupId=org.apache.pig -DartifactId=hadoop-common -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-common-$HADOOP_VERSION.jar
-mvn install:install-file -DgroupId=org.apache.pig -DartifactId=hadoop-client -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-client-$HADOOP_VERSION.jar
-mvn install:install-file -DgroupId=org.apache.pig -DartifactId=hadoop-hdfs -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-hdfs-$HADOOP_VERSION.jar
-mvn install:install-file -DgroupId=org.apache.pig -DartifactId=hadoop-minicluster -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-minicluster-$HADOOP_VERSION.jar
+mvn install:install-file -DgroupId=org.apache.hadoop -DartifactId=hadoop-common -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-common-$HADOOP_VERSION.jar
+mvn install:install-file -DgroupId=org.apache.hadoop -DartifactId=hadoop-client -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-client-$HADOOP_VERSION.jar
+mvn install:install-file -DgroupId=org.apache.hadoop -DartifactId=hadoop-hdfs -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-hdfs-$HADOOP_VERSION.jar
+mvn install:install-file -DgroupId=org.apache.hadoop -DartifactId=hadoop-minicluster -Dversion=$HADOOP_VERSION -Dpackaging=jar -Dfile=hadoop-minicluster-$HADOOP_VERSION.jar
 
 #Install pig jars
 mvn install:install-file -DgroupId=org.apache.pig -DartifactId=pig -Dversion=$PIG_VERSION -Dpackaging=jar -Dfile=pig-$PIG_VERSION.jar
