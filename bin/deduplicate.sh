02548436902ba0ee0464aa10be4ac07b81c7dbdd
Andrew Otto <acotto@gmail.com>
2013-06-26 18:15:45 -0400

    Fixing pig scrip tpath

--- bin/deduplicate.sh
+++ bin/deduplicate.sh
@@ -1,6 +1,6 @@
 # deduplicates webrequest log lines from $1.  In dirname $1/deduplicated
 source=$1
-pig_script="$(dirname $(dirname $0))/kraken/pig/deduplicate.pig"
+pig_script="$(dirname $(dirname $0))/pig/deduplicate.pig"
 
 hdfs_ls() {
     hadoop fs -ls $@ | sed -e '1d' | grep -vP '^Found .+ items' | awk '{print $NF}'
