7c17da6f1f9400a41dead0e7608520ec0d0f6453
Andrew Otto <acotto@gmail.com>
2013-06-26 18:13:12 -0400

    Updating deduplicate shell script

--- bin/deduplicate.sh
+++ bin/deduplicate.sh
@@ -1,19 +1,37 @@
-# deduplicates webrequest log lines from $1.  Stores output in $2
-
+# deduplicates webrequest log lines from $1.  In dirname $1/deduplicated
 source=$1
-dest=${2:-deduplicated}
+pig_script="$(dirname $(dirname $0))/kraken/pig/deduplicate.pig"
 
 hdfs_ls() {
     hadoop fs -ls $@ | sed -e '1d' | grep -vP '^Found .+ items' | awk '{print $NF}'
 }
 
+get_upper_time_bound() {
+    lower_time_bound=$1
+    frequency='15'  # minutes
+    python -c "
+from datetime import datetime, timedelta
+lower = datetime.strptime('$lower_time_bound', '%Y-%m-%d_%H.%M.%S')
+delta = timedelta(minutes=$frequency)
+upper = lower + delta
+print(upper.strftime('%Y-%m-%d_%H.%M.%S'))
+"
+}
+
 
 for f in $(hdfs_ls -d $source); do
+    # get the lower and upper time bounds
+    base_name=$(basename ${f})
+    lower_time_bound=${base_name:3} # strip off the dt= from the dirname
+    upper_time_bound=$(get_upper_time_bound $lower_time_bound)
+    
+    echo "-- $(date) Deduplicating $f ($lower_time_bound - $upper_time_bound) --"
     # get the bordering data
     files=$(echo $(hdfs_ls -d $source | grep -C 1 ${f}) | tr ' ' ',')
-    echo "-- $(date) Deduplicating $f --"
-    echo sudo -u hdfs pig -f /opt/kraken/pig/deduplicate.pig -p input="${files}" -p output="${dest}/deduplicated/$(basename $f)";
-    sudo -u hdfs pig -f /opt/kraken/pig/deduplicate.pig -p input="${files}" -p output="$(dirname $f)/deduplicated/$(basename $f)";
-    echo "  Done."
+    echo "   " sudo -u hdfs pig -f $pig_script -p lower_time_bound=${lower_time_bound} -p upper_time_bound=${upper_time_bound} -p input="${files}" -p output="$(dirname $f)/deduplicated/$(basename $f)";
+    sudo -u hdfs pig -f $pig_script -p lower_time_bound=${lower_time_bound} -p upper_time_bound=${upper_time_bound} -p input="${files}" -p output="$(dirname $f)/deduplicated/$(basename $f)";
+    echo "   Done."
     echo ''
 done
+
+
--- pig/deduplicate.pig
+++ pig/deduplicate.pig
@@ -2,11 +2,36 @@
 -- hostname,sequence,timestamp and bytes_sent.
 -- We cannot use 'DISTINCT' since the Kafka Byte Offset may
 -- be different if the duplicates existed in Kafka.
-SET default_parallism 2;
+REGISTER 'kraken-pig-0.0.2-SNAPSHOT.jar'
+
+%default date_format 'yyyy-MM-dd_hh.mm.ss';
+
+DEFINE DATE_CONVERT      org.wikimedia.analytics.kraken.pig.ConvertDateFormat('yyyy-MM-dd\'T\'HH:mm:ss', '$date_format');
 
 IMPORT 'include/load_webrequest.pig';
-
 LOG_FIELDS = LOAD_WEBREQUEST('$input');
+LOG_FIELDS = FOREACH LOG_FIELDS GENERATE
+  kafka_byte_offset,
+  hostname,
+  sequence,
+  timestamp,
+  request_time,
+  remote_addr,
+  http_status,
+  bytes_sent,
+  request_method,
+  uri,
+  proxy_host,
+  content_type,
+  referer,
+  x_forwarded_for,
+  user_agent,
+  accept_language,
+  x_cs,
+  DATE_CONVERT(timestamp)      AS dt:chararray;
+
+
+LOG_FIELDS = FILTER LOG_FIELDS BY (dt >= '$lower_time_bound' AND dt < '$upper_time_bound');
 
 GROUPED = GROUP LOG_FIELDS BY (hostname,sequence,timestamp,bytes_sent,uri);
 
@@ -14,5 +39,24 @@ FILTERED =  FOREACH GROUPED {
        LINE = LIMIT LOG_FIELDS 1;
        GENERATE FLATTEN(LINE);
 };
+
+FILTERED = FOREACH FILTERED GENERATE 
+  kafka_byte_offset,
+  hostname,
+  sequence,
+  timestamp,
+  request_time,
+  remote_addr,
+  http_status,
+  bytes_sent,
+  request_method,
+  uri,
+  proxy_host,
+  content_type,
+  referer,
+  x_forwarded_for,
+  user_agent,
+  accept_language,
+  x_cs;
 
 STORE FILTERED INTO '$output';
