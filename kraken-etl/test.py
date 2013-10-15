20d66707fc7d64e2cf76816d29a575d00530e39d
Andrew Otto <acotto@gmail.com>
2013-10-15 14:26:06 -0400

    Big commit containing complete hive-partitioner and pagecounts importer.
    Card: analytics 1195.1
    
    Change-Id: Ic8b10d1aa5a10729cc826e4eb82a5699e5f319ac

--- kraken-etl/test.py
+++ kraken-etl/test.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta
-from util import diffDatewise, timestampsToNow
+from util import HiveUtils, HdfsDatasetUtils, diff_datewise, timestamps_to_now, interval_hierarchies
 from unittest import TestCase
 
 
 class TestUtil(TestCase):
-    def testDiffDatewise(self):
+    def test_diff_datewise(self):
         l = []
-        lJustDates = []
+        l_just_dates = []
         r = []
         lp = 'blah%Y...%m...%d...%Hblahblah'
         rp = 'neenee%Y%m%d%Hneenee'
@@ -22,19 +22,19 @@ class TestUtil(TestCase):
                         x = datetime(y, m, d, h)
                         if not x in expect1:
                             l.append(datetime.strftime(x, lp))
-                            lJustDates.append(x)
+                            l_just_dates.append(x)
                         if not x in expect0:
                             r.append(datetime.strftime(x, rp))
         
-        result = diffDatewise(l, r, leftParse=lp, rightParse=rp)
+        result = diff_datewise(l, r, left_parse=lp, right_parse=rp)
         self.assertEqual(result[0], expect0)
         self.assertEqual(result[1], expect1)
         
-        result = diffDatewise(lJustDates, r, rightParse=rp)
+        result = diff_datewise(l_just_dates, r, right_parse=rp)
         self.assertEqual(result[0], expect0)
         self.assertEqual(result[1], expect1)
     
-    def testTimestampsToNow(self):
+    def test_timestamps_to_now(self):
         now = datetime.now()
         start = now - timedelta(hours=2)
         expect = [
@@ -42,5 +42,89 @@ class TestUtil(TestCase):
             start + timedelta(hours=1),
             start + timedelta(hours=2),
         ]
-        timestamps = timestampsToNow(start, timedelta(hours=1))
+        timestamps = timestamps_to_now(start, timedelta(hours=1))
         self.assertEqual(expect, list(timestamps))
+
+
+class TestHiveUtil(TestCase):
+    def setUp(self):
+        self.hive = HiveUtils()
+        self.hive.tables = {
+            'table1': {
+                'partitions': ['year=2013/month=10/day=01/hour=01', 'year=2013/month=10/day=01/hour=02'],
+                'interval':   'hourly',
+                'depth':      4,
+                'location':   '/path/to/data/table1/hourly',
+            },
+            'table2': {
+                'partitions': ['year=2013/month=10/day=01'],
+                'interval':   'daily',
+                'depth':      3,
+                'location':   '/path/to/data/table2/daily',
+            },
+        }
+
+    def test_table_exists(self):
+        self.assertTrue(self.hive.table_exists('table1'))
+        self.assertFalse(self.hive.table_exists('nonya'))
+
+    def test_partition_ddl(self):
+        d = datetime(2013,10,15,10)
+        for table in self.hive.tables.keys():
+
+            interval = self.hive.tables[table]['interval']
+            expect = 'PARTITION (%s) LOCATION \'%s/%s\'' % (
+                d.strftime(interval_hierarchies[interval]['hive_partition_ddl_format']),
+                self.hive.tables[table]['location'],
+                d.strftime(interval_hierarchies[interval]['directory_format'])
+            )
+            ddl = self.hive.partition_ddl(table, d)
+            self.assertEqual(ddl, expect)
+
+    def test_add_partitions_ddl(self):
+        table = 'table1'
+        interval = 'hourly'
+        ddl_format = interval_hierarchies[interval]['hive_partition_ddl_format']
+        ds = [datetime(2013,10,15,10), datetime(2013,10,15,11)]
+        expect = "ALTER TABLE table1 ADD\nPARTITION (%s),\nPARTITION (%s);" % (ds[0].strftime(ddl_format), ds[1].strftime(ddl_format))
+
+        expect_ddls = ['PARTITION (%s) LOCATION \'%s/%s\'' % (
+            d.strftime(interval_hierarchies[interval]['hive_partition_ddl_format']),
+            self.hive.tables[table]['location'],
+            d.strftime(interval_hierarchies[interval]['directory_format'])
+        ) for d in ds]
+
+        expect = 'ALTER TABLE ' + table + ' ADD\n' + '\n'.join(expect_ddls) + ';'
+
+        add_statement = self.hive.add_partitions_ddl(table, ds)
+        self.assertEqual(add_statement, expect)
+
+    def test_create_partitions(self):
+        table = 'table1'
+        ddl_format = interval_hierarchies['hourly']['hive_partition_ddl_format']
+        ds = [datetime(2013,10,15,10), datetime(2013,10,15,11)]
+        expect = "ALTER TABLE table1 ADD\nPARTITION (%s)\nPARTITION (%s);" % (ds[0].strftime(ddl_format), ds[1].strftime(ddl_format))
+
+    def test_partition_interval(self):
+        for t in self.hive.tables.keys():
+            expect = self.hive.tables[t]['interval']
+            self.assertEqual(self.hive.partition_interval(t), expect)
+
+    def table_location(self):
+        self.assertEqual(self.hive.table_location('table1'), self.hive.tables['table1']['location'])
+
+    def test_partition_location(self):
+        interval = 'hourly'
+        d = datetime(2013,10,15,10)
+        expect = self.hive.table_location('table1') + '/' + d.strftime(interval_hierarchies[interval]['directory_format'])
+        self.assertEqual(self.hive.partition_location('table1', d), expect)
+
+
+class TestHdfsDatasetUtils(TestCase):
+    def setUp(self):
+        self.hdfs = HdfsDatasetUtils('/path/to/data')
+
+    def test_dataset_dir(self):
+        self.assertEqual(self.hdfs.dataset_dir('nonya',   'hourly'), '/path/to/data/nonya/hourly')
+        self.assertEqual(self.hdfs.dataset_dir('boogers', 'yearly'), '/path/to/data/boogers/yearly')
+
--- kraken-etl/util.py
+++ kraken-etl/util.py
@@ -1,39 +1,46 @@
-import logger
+import logging
 from datetime import datetime
+import os
+import re
+import subprocess
 
 
 logger = logging.getLogger('kraken-etl-util')
 interval_hierarchies = {
     'hourly':  {
-        'depth': 4,
-        'directory_format': '%Y/%m/%d/%H',
-        'hive_partition_format': 'year=%Y/month=%m/day=%d/hour=%H'
+        'depth':                        4,
+        'directory_format':             '%Y/%m/%d/%H',
+        'hive_partition_format':        'year=%Y/month=%m/day=%d/hour=%H',
+        'hive_partition_ddl_format':    'year=%Y,month=%m,day=%d,hour=%H',
     },
     'daily':   {
-        'depth': 3,
-        'directory_format': '%Y/%m/%d',
-        'hive_partition_format': 'year=%Y/month=%m/day=%d'
+        'depth':                        3,
+        'directory_format':             '%Y/%m/%d',
+        'hive_partition_format':        'year=%Y/month=%m/day=%d',
+        'hive_partition_ddl_format':    'year=%Y,month=%m,day=%d',
     },
     'monthly': {
-        'depth': 2,
-        'directory_format': '%Y/%m'
-        'hive_partition_format': 'year=%Y/month=%m'
-    },
+        'depth':                        2,
+        'directory_format':             '%Y/%m',
+        'hive_partition_format':        'year=%Y/month=%m',
+        'hive_partition_ddl_format':    'year=%Y,month=%m',
+   },
     'yearly':  {
-        'depth': 1,
-        'directory_format': '%Y'
-        'hive_partition_format': 'year=%Y'
+        'depth':                        1,
+        'directory_format':             '%Y',
+        'hive_partition_format':        'year=%Y',
+        'hive_partition_ddl_format':    'year=%Y',
     },
 }
 
 
-def diffDatewise(left, right, leftParse=None, rightParse=None):
+def diff_datewise(left, right, left_parse=None, right_parse=None):
     """
     Parameters
         left        : a list of datetime strings or objects
         right       : a list of datetime strings or objects
-        leftParse   : None if left contains datetimes, or strptime format
-        rightParse  : None if right contains datetimes, or strptime format
+        left_parse   : None if left contains datetimes, or strptime format
+        right_parse  : None if right contains datetimes, or strptime format
 
     Returns
         A tuple of two sets:
@@ -41,26 +48,26 @@ def diffDatewise(left, right, leftParse=None, rightParse=None):
         [1] : the datetime objects in right but not left
     """
     
-    if leftParse:
-        leftSet = set([
-            datetime.strptime(l.strip(), leftParse)
+    if left_parse:
+        left_set = set([
+            datetime.strptime(l.strip(), left_parse)
             for l in left if len(l.strip())
         ])
     else:
-        leftSet = set(left)
+        left_set = set(left)
     
-    if rightParse:
-        rightSet = set([
-            datetime.strptime(r.strip(), rightParse)
+    if right_parse:
+        right_set = set([
+            datetime.strptime(r.strip(), right_parse)
             for r in right if len(r.strip())
         ])
     else:
-        rightSet = set(right)
+        right_set = set(right)
     
-    return (leftSet - rightSet, rightSet - leftSet)
+    return (left_set - right_set, right_set - left_set)
 
 
-def timestampsToNow(start, increment):
+def timestamps_to_now(start, increment):
     """
     Generates timestamps from @start to datetime.now(), by @increment
     
@@ -78,23 +85,25 @@ def timestampsToNow(start, increment):
         start += increment
 
 
-def sh(command):
+def sh(command, check_return_code=True):
     """
-    Execute a shell command and return the result
+    Execute a shell command and return the result.
+    command can be an array or a string.  If it is
+    a string, shell=True will be passed to subprocess.Popen.
     """
-    logger.debug('Running: {0}'.format(command))
-    return os.popen(command).read().strip()
 
+    if isinstance(command, list):
+        shell = False
+        command_string = ' '.join(command)
+    else:
+        shell = True
+        command_string = command
 
-def call(command, check_return_code=True):
-    """
-    Execute a shell command and return the result
-    """
-    logger.debug('Running: {0}'.format(' '.join(command)))
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logger.debug('Running: {0}'.format(command_string))
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=shell)
     stdout, stderr = p.communicate()
     if check_return_code and p.returncode != 0:
-        raise RuntimeError("Command: {0} failed with error code: {1}".format(" ".join(command), p.returncode),
+        raise RuntimeError("Command: {0} failed with error code: {1}".format(command_string, p.returncode),
                                stdout, stderr)
     return stdout.strip()
 
@@ -103,18 +112,59 @@ class HiveUtils(object):
     def __init__(self, database='default', options=''):
         self.database   = database
         self.options    = options.split()
-        self.hivecmd    = None
-        # list of partitions for table, keyed by table
-        # self.table_partitions = {}
         self.hivecmd = ['hive'] + self.options + ['cli', '--database', self.database]
-        # initialize tables dict with table names
         self.tables  = {}
-        # cache results for later
-        for t in self.tables_get():
-            self.tables[t] = {}
+
+
+
+    def tables_get(self):
+        """Returns a list of tables in the current database"""
+        t = self.query('SHOW TABLES').split('\n')
+        t.remove('tab_name')
+        return t
+
+
+    def tables_init(self):
+        """Initializes the self.tables dict"""
+
+        for table in self.tables_get():
+            self.tables[table] = {}
+
+        return self.tables
+
+
+    def table_exists(self, table): # ,force=False
+        """Returns true if the table exists in the current database."""
+        if not self.tables: self.tables_init()
+
+        return table in self.tables.keys()
+
+
+    def table_schema(self, table):
+        if not self.tables: self.tables_init()
+
+        if 'schema' not in self.tables[table].keys():
+            cmd = ' '.join(self.hivecmd) + ' -e \'SHOW CREATE TABLE ' + table + ';\' | sed \'1d\''
+            logger.debug('Running: %s' % cmd)
+            self.tables[table]['schema'] = sh(cmd)
+
+        return self.tables[table]['schema']
+
+
+    def table_location(self, table):
+        if not self.tables: self.tables_init()
+
+        if not 'location' in self.tables[table].keys():
+            location_pattern = re.compile(r"^LOCATION\n\s+'(.+)'\n", re.MULTILINE)
+            match = re.search(location_pattern, self.table_schema(table))
+            location = match.group(1)
+            self.tables[table]['location'] = location
+
+        return self.tables[table]['location']
 
     def partitions(self, table):
         """Returns a list of partitions for the given Hive table."""
+        if not self.tables: self.tables_init()
 
         # cache results for later
         # if we don't know the partitions yet, get them now
@@ -126,38 +176,85 @@ class HiveUtils(object):
 
         return self.tables[table]['partitions']
 
-    def table_exists(self, table): # ,force=False
-        return table in self.tables.keys()
 
-    def tables_get(self):
-        t = self.query('SHOW TABLES').split('\n')
-        t.remove('tab_name')
-        return t
+    def create_partitions(self, table, partition_datetimes):
+        """
+        Runs ALTER TABLE table ADD PARTITION ... for each of the partitions
+        defined in the partition_datetimes list.
+        """
+        if partition_datetimes:
+            q = self.add_partitions_ddl(table, partition_datetimes)
+            return self.query(q, True)
+        else:
+            logger.info("Not creating any partitions for table %s.  No partition datetimes were given." % table)
+
+
+    def add_partitions_ddl(self, table, partition_datetimes):
+        """
+        Returns a complete hive statement to add partitions to
+        table for the given datetimes.
+        """
+        ddls = [(self.partition_ddl(table, p)) for p in partition_datetimes]
+        ddls.sort()
+        return 'ALTER TABLE %s ADD\n%s;' % (table, '\n'.join(ddls))
+
+
+    def partition_ddl(self, table, partition_datetime):
+        """
+        Returns a portion of a partition DDL statement.
+        This statement is not enough to add a partition on its own,
+        but may be used as part of a larger ALTER TABLE ADD ... statement.
+        """
+        interval = self.partition_interval(table)
+        return 'PARTITION (%s) LOCATION \'%s\'' % (
+            partition_datetime.strftime(interval_hierarchies[interval]['hive_partition_ddl_format']),
+            self.partition_location(table, partition_datetime)
+        )
+
+    def partition_location(self, table, partition_datetime):
+        interval = self.partition_interval(table)
+        return self.table_location(table) + '/' + partition_datetime.strftime(interval_hierarchies[interval]['directory_format'])
+
 
     def partition_interval(self, table):
+        """
+        Examines the CREATE TABLE statements for partition layout.
+        Determines the time interval of the partitions based on
+        the number of partition levels.
+        This will cache the interval result in the self.tables dict.
+        """
+
+        if not self.tables: self.tables_init()
+
         intervals = {
             4: 'hourly',
             3: 'daily',
-            2: 'monhtly',
+            2: 'monthly',
             1: 'yearly',
         }
 
         # cache results for later
         if not 'interval' in self.tables[table].keys():
+
+            # TODO: Use self.table_schema and a regex 
+            # rather than running SHOW CREATE TABLE AGAIN!
+
             # counts the number of partition keys this table has
             # and returns a string time interval based in this number        
             cmd = ' '.join(self.hivecmd) + ' -e \'SHOW CREATE TABLE ' + table + ';\' | sed -n \'/PARTITIONED BY (/,/)/p\' | grep -v \'PARTITIONED BY\' | wc -l'
             logger.debug('Running: %s' % cmd)
-            # using check_output directly here so that we can pass shell=True and use pipes.
-            partition_depth = int(subprocess.check_output(cmd, stderr=subprocess.PIPE, shell=True).strip())
+            partition_depth = int(sh(cmd))
+
             self.tables[table]['depth']    = partition_depth
             self.tables[table]['interval'] = intervals[partition_depth]
 
         return self.tables[table]['interval']
 
+
     def query(self, query, check_return_code=True):
         """Runs the given hive query and returns stdout"""
         return self.command(['-e', query], check_return_code)
+
 
     def script(self, script, check_return_code=True):
         """Runs the contents of the given script in hive and returns stdout"""
@@ -165,55 +262,68 @@ class HiveUtils(object):
             raise RuntimeError("Hive script: {0} does not exist.".format(script))
         return self.command( ['-f', script], check_return_code)
 
+
     def command(self, args, check_return_code=True):
         """Runs the `hive` from the command line, passing in the given args, and
            returning stdout.
         """
         cmd = self.hivecmd + args
-        return call(cmd, check_return_code)
+        return sh(cmd, check_return_code)
 
 
-class CamusUtils(object):
+class HdfsDatasetUtils(object):
     """
-    Deal with topics and partitions created by Camus
-    """
-    def __init__(self, destination_path):
-        self.destination_path = destination_path
+    Deal with time partitioned data imports in HDFS.
+    Data directory hierarchies must be of the form:
+      <datadir>/<dataset>/<interval>/<year>/<month>/<day>/<hour>.
 
-    def topics(self):
-        """Reads topic names out of destination_path"""
+    <datadir> is the HDFS path of all of your data imports.
+    <dataset> is the name of the dataset inside of <datadir>
+
+    Intervals supported:
+      hourly
+      monthly
+      daily
+      yearly
+
+    The depth of the hierarchy is dependent on the <interval>.
+    E.g. monthly will only be 2 directories deep (<year>/<month>), etc.
+
+    This is useful for identifying time buckets created by Camus,
+    LinkedIn's Kafka -> HDFS import framework.
+    """
+    def __init__(self, datadir):
+        self.datadir = datadir
+
+    def datasets(self):
+        """Reads dataset names out of <datadir>"""
         return sh(
             'hdfs dfs -ls {0}/ | '\
             'grep -v "Found .* items" | '\
             'awk -F "/" \'{{print $NF}}\''.format(
-                self.destination_path
+                self.datadir
             )
         ).split('\n')
 
-    def basedir(self, topic, interval='hourly'):
-        return self.destination_path + '/' + topic + '/' + interval
+    def dataset_dir(self, dataset, interval='hourly'):
+        return (self.datadir + '/' + dataset + '/' + interval)
 
-    # TODO configure interval partition paths automatically instead of hardcoding
-    def partitions(self, topic, interval='hourly'):
+    # TODO configure interval partition paths automatically instead of hardcoding with time buckets
+    def partitions(self, dataset, interval='hourly'):
         """
-        Returns a list of time bucketd 'partitions' created by Camus import.
-        These are inferred from directories in hdfs.
+        Returns a list of time bucketed 'partitions' in HDFS for this dataset.
+        These are inferred from the directory hierarchy in HDFS.
         """
 
-        basedir = self.basedir(topic, interval)
+        basedir = self.dataset_dir(dataset, interval)
 
-        depth = interval_hierarchies[interval]['depth']
         # number of  wildcard time bucket directories, e.g. depth_stars == 4 -> '/*/*/*/*'
-        depth_stars = '/*' * depth
+        depth_stars = '/*' * interval_hierarchies[interval]['depth']
         directories = sh(
             'hdfs dfs -ls -d {0}{1} | '\
             'grep -v \'Found .* items\' | '\
             'awk \'{{print $NF}}\''.format(basedir, depth_stars)
         ).split('\n')
+
         # return list of time bucket directories, e.g. ['2013/10/09/15', '2013/10/09/16', ... ]
         return [directory.replace(basedir + '/', '') for directory in directories]
-
-        # return sh('hdfs dfs -ls -R {0}/{1}/{2} | sed "s@.*{0}/{1}/{2}/\([0-9]*\)/\([0-9]*\)/\([0-9]*\)/\([0-9]*\)/.*@year=\1, month=\2, day=\3, hour=\4@" | grep "year.*" | sort | uniq'.format(self.destination_path, topic, interval)).split()
-        # return a list of lists of time buckets, e.g. [['2013']]
-        # return [directory.split('/')[-depth:] for directory in directories]    
-        # return [(int(d) for d in directory.split('/')[-depth:]) for directory in directories]
